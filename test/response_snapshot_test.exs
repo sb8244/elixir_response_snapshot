91d56fef694ddf3b6b453f4432791e8d1aec6f4b
Stephen Bussey <sb8244@cs.ship.edu>
2018-04-15 14:08:20 -0400

    exact existing matches should do nothing

--- test/response_snapshot_test.exs
+++ test/response_snapshot_test.exs
@@ -6,11 +6,17 @@ defmodule ResponseSnapshotTest do
   describe "store_and_compare!/2" do
     test "a new path writes the data to disk" do
       path = "test/fixtures/integration_new.json"
-      %{a: 1, b: 2}
-        |> ResponseSnapshot.store_and_compare!(path: path)
+      %{a: 1, b: 2} |> ResponseSnapshot.store_and_compare!(path: path)
 
       %{"data" => %{"a" => 1, "b" => 2}} = FileManager.read_fixture(path)
       FileManager.cleanup_fixture(path)
+    end
+
+    test "an existing fixture which matches does not alter the file on disk nor raise" do
+      path = "test/fixtures/integration_existing.json"
+      original_fixture = FileManager.read_fixture(path)
+      %{a: 1, b: 2} |> ResponseSnapshot.store_and_compare!(path: path)
+      assert FileManager.read_fixture(path) == original_fixture
     end
   end
 end
