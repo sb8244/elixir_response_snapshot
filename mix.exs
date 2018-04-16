26483bb625f9e8a6f4022cbdb53109f445d20a3f
Stephen Bussey <sb8244@cs.ship.edu>
2018-04-16 13:09:01 -0400

    clean up for publishing

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ResponseSnapshot.MixProject do
 
   def project do
     [
-      app: :elixir_response_snapshot,
+      app: :response_snapshot,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
