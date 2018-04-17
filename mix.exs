ebce03551666ff6ea67395719e917b88ad04e841
Stephen Bussey <sb8244@cs.ship.edu>
2018-04-16 23:33:55 -0400

    v1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ResponseSnapshot.MixProject do
   def project do
     [
       app: :response_snapshot,
-      version: "0.1.3",
+      version: "1.0.0",
       elixir: "~> 1.6",
       description: description(),
       package: package(),
